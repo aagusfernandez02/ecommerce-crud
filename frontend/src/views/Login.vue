36ae5a55c52256898fb1f8b2da773dc11c67a733
Agustín Fernandez <af.agusfernandez02@gmail.com>
2025-11-24 12:05:38 -0300

    frontend: login page > showPassword / hidePassword

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -5,14 +5,15 @@ import { login } from '@/services/api';
 const valid = ref(false);
 const username = ref('');
 const password = ref('');
+const showPassword = ref(false);
 
 const usernameRule = [
-    v => !!v || 'Username is required', 
+    v => !!v || 'Username is required',
     v => v.length >= 4 || 'Username must be at least 4 characters'
 ];
 
 const passwordRule = [
-    v => !!v || 'Password is required', 
+    v => !!v || 'Password is required',
     v => v.length >= 6 || 'Password must be at least 6 characters'
 ];
 
@@ -20,31 +21,39 @@ const handleSubmit = async () => {
     console.log("Handle submit!")
     await login(username.value, password.value);
 }
+
+const handleShowPasswordClick = () => {
+    showPassword.value = !showPassword.value;
+}
 </script>
 
 <template>
     <main>
         <v-card class="login-card">
             <v-card-title class="text-center pb-2">Bienvenido al ecommerce</v-card-title>
-            <v-card-subtitle class="text-center pb-5">Ingrese su usuario y contraseña para iniciar sesión</v-card-subtitle>
+            <v-card-subtitle class="text-center pb-5">Ingrese su usuario y contraseña para iniciar
+                sesión</v-card-subtitle>
             <v-card-text>
                 <v-form v-model="valid">
                     <v-row class="px-3">
-                        <v-text-field v-model="username" :rules="usernameRule" label="Username" clearable clear-icon="mdi-close"></v-text-field>
+                        <v-text-field v-model="username" :rules="usernameRule" label="Username" clearable
+                            clear-icon="mdi-close"></v-text-field>
                     </v-row>
                     <v-row class="px-3 pt-1">
-                        <v-text-field v-model="password" :rules="passwordRule" label="Password" clearable clear-icon="mdi-close"></v-text-field>
+                        <v-text-field v-model="password" :rules="passwordRule" label="Password" clearable clear-icon="mdi-close" :type="showPassword ? 'text' : 'password'">
+                            <template v-slot:append >
+                                <v-icon @click="handleShowPasswordClick">
+                                    {{ showPassword ? 'mdi-eye-off' : 'mdi-eye' }}
+                                </v-icon>
+                            </template>
+                        </v-text-field>
                     </v-row>
                 </v-form>
             </v-card-text>
             <v-card-actions>
-                <v-btn 
-                    :disabled="!valid"
-                    @click="handleSubmit"
-                    color="#624CAB" 
-                    variant="tonal" 
-                    class="ml-auto mr-0"
-                >Iniciar sesión</v-btn>
+                <v-btn :disabled="!valid" @click="handleSubmit" color="#624CAB" variant="tonal"
+                    class="ml-auto mr-0">Iniciar
+                    sesión</v-btn>
             </v-card-actions>
         </v-card>
     </main>
