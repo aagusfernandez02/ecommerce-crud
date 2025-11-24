06c0cbac09982c82b1c72dda4f030ec8e32870c6
Agustín Fernandez <af.agusfernandez02@gmail.com>
2025-11-24 11:09:07 -0300

    frontend: import toastify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,13 +11,22 @@ import '@mdi/font/css/materialdesignicons.css'
 // Router
 import router from './router/router'
 
+// Toastify
+import { createToastify } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 
 const app = createApp(App)
 const vuetify = createVuetify({
   components,
   directives,
 })
+const toastify = createToastify({
+  position: 'top-right',
+  autoClose: 2500,
+});
+
 app.use(vuetify)
 app.use(router)
+app.use(toastify);
 app.mount('#app')
 
